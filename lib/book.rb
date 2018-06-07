5b6fb1ce12319a471e5d9be39e73bad965cbe538
JewelsCP <giulianacarballoparker@gmail.com>
2018-06-07 22:15:46 +0000

    automatically backed up by learn

--- lib/book.rb
+++ lib/book.rb
@@ -2,4 +2,17 @@
 # Add you Book class here
 
 class Book
+  
+  def initialize(title)
+    @title  = title 
+  end 
+  
+  def title
+    @title
+  end
+  
+  def author = (author)
+    @author = author
+  end
+  
 end 
